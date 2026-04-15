0cfb19e39aea8374782ed8655d84558b283a5689
Chanchal Choudhary <216878440+chanchal434@users.noreply.github.com>
2026-04-15 19:43:11 +0530

    Fix shutdown message wording

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def start():
     try:
         eel.start("index.html", mode=None, host='localhost', block=True)
     except (SystemExit, KeyboardInterrupt):
-        print("Prajñāvan AI is shutting down...")
+        print("Prajñāvan AI is going to shutting down...")
 
 if __name__ == '__main__':
     start()
